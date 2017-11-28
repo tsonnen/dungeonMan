f8559f87762228807997668b812c64c59f71a9be
Tim Sonnen <tsonnen67@gmail.com>
2017-11-28 01:13:39 -0800

    Remove garbage

--- core/src/edu/cs328/game/Dungeon.java
+++ core/src/edu/cs328/game/Dungeon.java
@@ -63,6 +63,7 @@ public class Dungeon {
 			}
 		}
 		walls.setName("walls");
+		
 		TiledMapTileLayer enemies = new TiledMapTileLayer(width, height, tileWidth, tileHeight);
 		Texture wallTexture = new Texture(Gdx.files.internal("wall.png"));
 		Cell cell = new Cell();
@@ -72,10 +73,8 @@ public class Dungeon {
 		enemies.setCell(x + 4, y + 4, cell);
 		layers.add(enemies);
 		layers.add(walls);
-		//TiledMapTileLayer treasureLayer = placeTreasure(width, height, walls);
-		//placeEnemy(width, height, walls, treasureLayer);
-		//getSpawn(width, height, walls);
-		spawn.set(5,5);
+
+		spawn.set(roomWidth/2,roomHeight/2);
 	}
 
 	private TiledMapTileLayer makeRoom(int startX, int startY, TiledMapTileLayer layer){
@@ -109,135 +108,5 @@ public class Dungeon {
 			}
 		}
 		return layer;
-	}
-
-	public TiledMapTileLayer doSimulation(int width, int height, TiledMapTileLayer layer, Cell cell){
-		TiledMapTileLayer newLayer = new TiledMapTileLayer(width, height, 16, 16);
-		for(int x = 0; x<width; x++){
-        	for(int y = 0; y < height; y++){
-            	int nbs = countAliveNeighbours(x, y, width, height, layer);
-            	//The new value is based on our simulation rules
-            	//First, if a cell is alive but has too few neighbours, kill it.
-            	Cell neighbour = layer.getCell(x,y);
-            	if(neighbour != null){
-                	if(nbs > deathLimit){
-                    	newLayer.setCell(x, y, cell);
-                	}
-            	} //Otherwise, if the cell is dead now, check if it has the right number of neighbours to be 'born'
-            	else{
-                	if(nbs > birthLimit){
-                    	newLayer.setCell(x, y, cell);
-                	}
-            	}
-        	}
-    	}
-        return newLayer;
-	}
-
-	private void placeEnemy(int width, int height, TiledMapTileLayer wallLayer, TiledMapTileLayer treasureLayer){
-		Texture texture = new Texture(Gdx.files.internal("treasure.png"));
-		TiledMapTileLayer enemies = new TiledMapTileLayer(width, height, 16, 16);
-		Cell cell = new Cell();
-		cell.setTile(new StaticTiledMapTile(new TextureRegion(texture)));
-
-		int treasureHiddenLimit = 5;
-    	for (int x=0; x < width; x++){
-        	for (int y=0; y < height; y++){
-        		Cell neighbour = wallLayer.getCell(x,y);
-            	if(neighbour == null){
-            		neighbour = treasureLayer.getCell(x,y);
-            		if(neighbour == null){
-                		int nbs = countAliveNeighbours(x, y, width, height, wallLayer) + countAliveNeighbours(x, y, width, height, treasureLayer);
-                		if(nbs >= treasureHiddenLimit){
-                    		enemies.setCell(x, y, cell);
-                		}
-                	}
-            	}
-        	}
-    	}
-    	enemies.setVisible(false);
-    	enemies.setName("enemies");
-    	layers.add(enemies);
-	}
-
-	public int countAliveNeighbours(int x, int y, int width, int height, TiledMapTileLayer countLayer){
-    	int count = 0;
-    	for(int i=-1; i<2; i++){
-        	for(int j=-1; j<2; j++){
-            	int neighbour_x = x+i;
-            	int neighbour_y = y+j;
-            	Cell neighbour = countLayer.getCell(neighbour_x, neighbour_y);
-            	//If we're looking at the middle point
-            	if(i == 0 && j == 0){
-                	//Do nothing, we don't want to add ourselves in!
-            	}
-            	//In case the index we're looking at it off the edge of the map
-            	else if(neighbour_x < 1 || neighbour_y < 1 || neighbour_x >= width - 1 || neighbour_y >= height - 1){
-                	count = count + 1;
-            	}
-            	//Otherwise, a normal check of the neighbour
-            	else if(neighbour != null){
-                	count = count + 1;
-            	}
-        	}
-    	}
-    	return count;
-	}
-
-	private TiledMapTileLayer placeTreasure(int width, int height, TiledMapTileLayer wallLayer){
-		Texture treasureTexture = new Texture(Gdx.files.internal("treasure.png"));
-		TiledMapTileLayer treasure = new TiledMapTileLayer(width, height, 16, 16);
-		Cell cell = new Cell();
-		cell.setTile(new StaticTiledMapTile(new TextureRegion(treasureTexture)));
-
-		int treasureHiddenLimit = 5;
-    	for (int x=0; x < width; x++){
-        	for (int y=0; y < height; y++){
-        		Cell neighbour = wallLayer.getCell(x,y);
-            	if(neighbour == null){
-                	int nbs = countAliveNeighbours(x, y, width, height, wallLayer);
-                	if(nbs >= treasureHiddenLimit){
-                    	treasure.setCell(x, y, cell);
-                	}
-            	}
-        	}
-    	}
-    	treasure.setName("treasure");
-    	layers.add(treasure);
-    	return treasure;
-	}
-
-	public boolean hitTreasure(int x, int y, Rectangle hit){
-		TiledMapTileLayer layer = (TiledMapTileLayer)layers.get("treasure");
-		for(int i = -1; i < 2; i++){
-			for(int j = -1; j < 3; j++){
-				Rectangle tile = new Rectangle(x + i, y + j,1,1);
-				Cell cell = layer.getCell(x + i, y + j);
-				if(tile.overlaps(hit) && cell != null){
-					layer.setCell(x + i, y + j,null);
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-
-	private void getSpawn(int width, int height, TiledMapTileLayer layer){
-		int x = width/2;
-		int y = height/2;
-		Cell spawnLoc = layer.getCell(x, y);
-		int mod = 1;
-		while(spawnLoc != null){
-			if(Math.random() < .5){
-				mod = -1;
-			}
-			if(Math.random() < .5){
-				x += mod;
-			}else{
-				y += mod;
-			}
-			spawnLoc = layer.getCell(x, y);
-		}
-		spawn.set(x,y);
 	}
 }
