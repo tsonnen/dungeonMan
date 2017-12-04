9542d08d75438197dc5a621d7f41d2f6175576b7
Tim Sonnen <tsonnen67@gmail.com>
2017-12-04 12:01:34 -0800

    Remove un-needed debug

--- core/src/edu/cs328/game/LoseScreen.java
+++ core/src/edu/cs328/game/LoseScreen.java
@@ -43,8 +43,6 @@ public class LoseScreen implements Screen, InputProcessor {
         // clear the screen ready for next set of images to be drawn
         Gdx.gl.glClearColor(0f, 0f, 0f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        System.out.println(Gdx.graphics.getWidth());
-        System.out.println(Gdx.graphics.getHeight());
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
         batch.draw(helpTexture, 0,0, 640, 480);
